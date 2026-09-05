425221f770148f29f53ae23da6d380bd9be24e10
murlishankar458 <murlishankar458@gmail.com>
2026-09-05 18:56:55 +0530

    Add files via upload

---
+++ Dice_roll.py
@@ -0,0 +1,48 @@
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value,max_value)
+    return roll
+
+value = roll()
+print(value)
+
+while True:
+    players = input("Enter the number of players (2-4)")
+    if players.isdigit():
+        players = int(players)
+
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2-4 players")
+    else:
+         print("Invalid! Try Again")
+
+print(players)
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\nplayer",player_idx+1 , "turn has just started!\n")
+        current_score = 0
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+            else:
+                print("you rolled a", value )
+
+            print("your current score is",current_score)
+
+        player_scores[player_idx] += current_score  
+        print("Your total score is:", player_scores[player_idx])
